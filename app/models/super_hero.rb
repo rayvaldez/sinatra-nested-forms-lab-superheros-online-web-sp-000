a4877a3e54e3b4e959ae4d221c3bde0e53540734
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-12-19 21:28:17 +0000

    Automatically backed up by Learn

---
+++ app/models/super_hero.rb
@@ -0,0 +1,15 @@
+class Superhero
+  attr_reader :name, :power, :bio
+  SUPERHEROES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    SUPERHEROES << self
+  end
+
+  def self.all
+    SUPERHEROES
+  end
+end
